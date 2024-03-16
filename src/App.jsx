6f5f34c6863d46da43f1e4cc1d59a8a01de1acca
nikumyash <98515855+nikumyash@users.noreply.github.com>
2024-03-16 16:30:07 +0530

    background animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Themes from "./components/Themes"
+import ParticleBackground from 'react-particle-backgrounds'
 
 const App = () => {
+  const settings = {
+    canvas: {
+      canvasFillSpace: true,
+      width: 200,
+      height: 200,
+      useBouncyWalls: false
+    },
+    particle: {
+      particleCount: 200,
+      color: '#94ecbe',
+      minSize: 2,
+      maxSize: 4
+    },
+    velocity: {
+      directionAngle: 0,
+      directionAngleVariance: 360,
+      minSpeed: 2,
+      maxSpeed: 4
+    },
+    opacity: {
+      minOpacity: 0,
+      maxOpacity: 0.5,
+      opacityTransitionTime: 20000
+    }
+  }
+
   return (
-    <div className="w-full text-white font-quicksand bg-black min-h-screen">
-      <main className="max-w-7xl m-auto">
+    <div className="w-full relative text-white font-quicksand bg-black min-h-screen">
+      <div className="absolute z-0 w-screen h-full">
+        <ParticleBackground settings={settings} />
+      </div>
+      <main className="max-w-7xl z-10 mx-auto ">
           <Navbar/>
           <Hero/>
           <Themes/>
--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -3,11 +3,11 @@ import {motion} from "framer-motion"
 
 function Images({ text, url }) {
     return (
-      <motion.section className="my-24 flex justify-around items-center">
-        <motion.div transition={{duration:0.2}} variants={{hidden:{scale:0},visible:{scale:1}}} initial="hidden" whileInView="visible" className="object-contain overflow-hidden h-96 w-96" >
+      <motion.section className="lg:mt-24 mt-4 flex-col lg:flex-row flex justify-around items-center">
+        <motion.div transition={{duration:0.2}} variants={{hidden:{scale:0},visible:{scale:1}}} initial="hidden" whileInView="visible" className="object-contain hover:opacity-50 overflow-hidden h-96 w-96" >
           <img className="h-full" src={url} alt={text} />
         </motion.div>
-        <motion.h2 variants={{hidden:{y:100},visible:{y:0}}} transition={{duration:0.2}} initial="hidden" whileInView="visible" className="text-blue-600 text-center text-5xl">
+        <motion.h2 variants={{hidden:{y:100},visible:{y:0},hover:{scale:1.25}}} transition={{duration:0.2}} whileHover="hover" initial="hidden" whileInView="visible" className="text-blue-600 text-center my-4 text-2xl lg:text-5xl">
             {text}
         </motion.h2>
       </motion.section>
@@ -16,8 +16,8 @@ function Images({ text, url }) {
   
 const Themes = () => {
   return (
-    <section className="w-full my-24 px-4 min-h-screen">  
-        <h1 className="text-7xl tracking-wider text-center">
+    <section className="w-full ">  
+        <h1 className="text-4xl font-serif lg:text-7xl my-4 tracking-wider text-center">
             Events
         </h1>
         <Images text="Robotics and Automation" url="/1.webp"/>
